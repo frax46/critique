40a91c163496f2841735919255686360ddfe355e
FRANCE ANNOBIL <annobilfrance@gmail.com>
2024-10-30 06:36:16 +0000

    subscribe page active subsciption added

--- app/actions/userActions.ts
+++ app/actions/userActions.ts
@@ -2,16 +2,32 @@ import prisma from "@/lib/prisma";
 import { auth, clerkClient } from "@clerk/nextjs/server";
 
 export const getUser = async () => {
-    const { userId } = auth();
-    
-    if (!userId) {
-      return null;
-    }
+  const { userId } = auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const clerkUser = await clerkClient().users.getUser(userId);
+
+  const user = await prisma.user.findUnique({
+    where: { email: clerkUser.emailAddresses[0].emailAddress },
+  });
+  return user;
+};
+
+export const getUserSubscriptions = async () => {
+  const user = await getUser();
+  if (!user) return null;
   
-    const clerkUser = await clerkClient().users.getUser(userId);
-    
-    const user = await prisma.user.findUnique({
-      where: { email: clerkUser.emailAddresses[0].emailAddress },
-    });
-    return user;
-  };
+  const subscriptions = await prisma.subscription.findMany({
+    where: { 
+      userId: user.id,
+      expiresAt: {
+        gt: new Date() // Only get subscriptions that haven't expired
+      }
+    }
+  });
+  return subscriptions;
+};
+
